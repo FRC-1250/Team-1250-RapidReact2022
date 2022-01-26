5579c0ad0427157aeed6c35ad553ec5ba85dab82
tars-1250 <gatorbottars1250@gmail.com>
2022-01-26 18:28:55 -0500

    Sortball work
    Created color for matching, created methods for sorting ball.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Sorter;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -16,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-
+private final Sorter m_sorter=new Sorter();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
--- src/main/java/frc/robot/subsystems/Sorter.java
+++ src/main/java/frc/robot/subsystems/Sorter.java
@@ -5,8 +5,14 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorSensorV3;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -14,8 +20,16 @@ public class Sorter extends SubsystemBase {
   CANSparkMax sorter = new CANSparkMax(Constants.sorter_CAN_ID, MotorType.kBrushless);
   CANSparkMax sorterCollect = new CANSparkMax(Constants.sorterCollect_CAN_ID, MotorType.kBrushless);
 
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  public final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  public final Color kBlueTarget = new Color(0.25, 0.29, 0.45);
+  public final Color kRedTarget = new Color(0.60, 0.32, 0.07);
+  public final ColorMatch m_colorMatcher = new ColorMatch();
+
   /** Creates a new Sorter. */
   public Sorter() {
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
   }
 
   public void Setsortercollectspeed(double speed) {
