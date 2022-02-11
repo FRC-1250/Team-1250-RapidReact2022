adce74aa181db9bb4bc1278a6bc1b1735a81640c
tars-1250 <89310954+tars-1250@users.noreply.github.com>
2022-02-11 18:55:52 -0500

    Added new commands ExtendIntake and RetractIntake

--- src/main/java/frc/robot/commands/ExtendIntake.java
+++ src/main/java/frc/robot/commands/ExtendIntake.java
@@ -4,17 +4,42 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ExtendIntake extends InstantCommand {
-  public ExtendIntake() {
-    // Use addRequirements() here to declare subsystem dependencies.
+public class ExtendIntake extends CommandBase {
+  private final Intake intake; 
+  public ExtendIntake(Intake m_intake){
+    intake = m_intake;
   }
+   
+
+  
+  /** Creates a new ExtendIntake. */
+  
+    
+    // Use addRequirements() here to declare subsystem dependencies.
+  
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    intake.SetIntakeRollerspeed(.5);
+    intake.setIntakeDeploySpeed(.5);
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return intake.isRightForwardLimitSwitchPressed();
+  }
 }
