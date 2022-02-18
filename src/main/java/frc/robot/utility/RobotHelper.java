30947b1747d7ae682e38d6c59a6084e720e0909f
Rodney Lewis <rodneylewis95@gmail.com>
2022-02-18 08:17:33 -0500

    Update RobotHelper.java
    - Added java docs for helper methods
    - New linear ramp function

--- src/main/java/frc/robot/utility/RobotHelper.java
+++ src/main/java/frc/robot/utility/RobotHelper.java
@@ -41,6 +41,13 @@ public class RobotHelper {
         }
     }
 
+    /**
+     * 
+     * @param inputValue the value checked against the threshold to be returned
+     * @param threshold  the threshold value that must be breached for the
+     *                   inputValue be returned
+     * @return 0 or the inputValue if it is above or below the provided threshold
+     */
     public static double Deadband(double inputValue, double threshold) {
         if (inputValue >= +threshold || inputValue <= -threshold) {
             return inputValue;
@@ -48,6 +55,15 @@ public class RobotHelper {
             return 0;
         }
     }
+
+    /**
+     * 
+     * @param inputValue the value checked against the threshold to be returned
+     * @param threshold  the threshold value that must be not breached for the
+     *                   inputValue to be returned
+     * @return the inputValue or the threshold if the inputValue is greater or less
+     *         than the threshold
+     */
 
     public static double Limit(double inputValue, double threshold) {
         if (inputValue >= +threshold) {
@@ -57,5 +73,70 @@ public class RobotHelper {
         } else {
             return inputValue;
         }
+    }
+
+    /**
+     * 
+     * @param upperSpeed the upper speed limit to use in the piecewise function,
+     *                   between -1 and 1. If negative, it should be less than the
+     *                   lower speed limit. If positive, it should greater than the
+     *                   lower speed limit.
+     * @param lowerSpeed the lower speed limit to use in the piecewise function,
+     *                   between -1 and 1. If negative, it should be greater than
+     *                   the upper speed limit. If positive, it should less than the
+     *                   upper speed limit.
+     * @param start      the value of the position where the motor or device is
+     *                   starting (ticks, angle, rotations).
+     * @param end        the value of the position where the motor or device needs
+     *                   to be (ticks, angle,rotations).
+     * @param current    the value of the position where the motor or device
+     *                   currently is (ticks, angle, rotations).
+     * @return a motor output of the upper speed limit that transitions into the
+     *         lower speed limit through a linear ramp that is biased to negative
+     *         being "forward". i.e. the upper speed will be
+     *         used for the first 70% of the route, than a speed between the upper
+     *         and lower speed limits between 71% to 89% and then from 90% to 100%
+     *         the lower speed limit.
+     */
+
+    public static double piecewiseMotorController(double upperSpeed, double lowerSpeed, double start,
+            double end, double current) {
+        return piecewiseMotorController(upperSpeed, 0.7, lowerSpeed, 0.9, start, end, current);
+    }
+
+    private static double piecewiseMotorController(double upperSpeed, double rampStartPercent,
+            double lowerSpeed, double rampEndPercent, double start, double end, double current) {
+        double diffTotal = Math.abs(start - end);
+        double progressPercent = Math.abs((start - current) / diffTotal);
+        boolean isNegative = Math.signum(start - end) == -1;
+        double speed = 0;
+
+        if (progressPercent > rampStartPercent && progressPercent < rampEndPercent) {
+            speed = linearRamp(upperSpeed, rampStartPercent, lowerSpeed, rampEndPercent, progressPercent);
+        } else if (progressPercent >= rampEndPercent && progressPercent <= 100) {
+            speed = lowerSpeed;
+        } else if (progressPercent >= 0 && progressPercent <= rampStartPercent) {
+            speed = upperSpeed;
+        }
+
+        // Robots forward = negative
+        if (isNegative) {
+            return -speed;
+        } else {
+            return speed;
+        }
+    }
+
+    private static double linearRamp(double upperSpeed, double rampStartPercent, double lowerSpeed,
+            double rampEndPercent, double progressPercent) {
+        double slope = 0;
+        double b = 0;
+        double y = 0;
+
+        slope = (upperSpeed - lowerSpeed) / (rampStartPercent - rampEndPercent);
+        b = upperSpeed - (slope * rampStartPercent);
+        y = slope * progressPercent + b;
+
+        return y;
     }
 }
